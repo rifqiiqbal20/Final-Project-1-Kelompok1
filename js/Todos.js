60677edb00af64c9653f383cbff27fb86dceb073
Rifqi Iqbal <88646829+rifqiiqbal20@users.noreply.github.com>
2023-04-28 20:00:40 +0700

    Update Todos.js

--- js/Todos.js
+++ js/Todos.js
@@ -19,6 +19,7 @@ function displayDate(){
   document.querySelector("#date").innerHTML = date 
 }
 
+
 function displayItems(){
   let items = ""
   for(let i = 0; i < itemsArray.length; i++){
@@ -26,17 +27,16 @@ function displayItems(){
                 <div class="input-controller">
                   <textarea disabled>${itemsArray[i]}</textarea>
                   <div class="edit-controller">
-                  <i class="deleteBtn">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/></svg>             </div>
-  </i>             
-  </div>
+                    <i class="fa-solid fa-check deleteBtn"></i>
+                  </div>
+                </div>
+                <div class="update-controller">
+                 
                 </div>
               </div>`
   }
   document.querySelector(".to-do-list").innerHTML = items
   activateDeleteListeners()
-
 }
 
 function activateDeleteListeners(){
